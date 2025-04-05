038883170f8e8815380fceee1f60dbabc6aa0014
B1nus <119787571+B1nus@users.noreply.github.com>
2025-04-05 14:50:40 +0200

    shader

--- scripts/global.gd
+++ scripts/global.gd
@@ -7,9 +7,15 @@ var did_nothing: bool = true
 var bad_value: float = 0.0
 
 
-var bad_value_map: Dictionary[Chemical, float] = {
+const bad_value_map: Dictionary[Chemical, float] = {
 	Chemical.PFAS: 0.1,
 	Chemical.BEEWAX: 0.0,
+}
+
+
+const modulate_map: Dictionary[Chemical, Color] = {
+	Chemical.PFAS: Color.WHITE,
+	Chemical.BEEWAX: Color.INDIAN_RED,
 }
 
 
--- scripts/spray_bottle.gd
+++ scripts/spray_bottle.gd
@@ -11,6 +11,8 @@ const click_distance: float = 40.0
 
 func _process(delta: float) -> void:
 	$AnimatedSprite2D.use_parent_material = false
+	$AnimatedSprite2D.modulate = Global.modulate_map[chemical]
+	modulate = Global.modulate_map[chemical]
 	if Global.chemical == chemical:
 		z_index = 1
 		global_position = get_global_mouse_position() + offset
