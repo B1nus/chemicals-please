c9a109bf1a784b30a2315fe728cda6030176094e
B1nus <119787571+B1nus@users.noreply.github.com>
2025-04-06 12:05:35 +0200

    Spray bottles

--- scripts/spray_bottle.gd
+++ scripts/spray_bottle.gd
@@ -14,7 +14,7 @@ func _process(delta: float) -> void:
 	$Sprite2D.use_parent_material = false
 	$Sprite2D.texture = idle_image
 	if Global.chemical == chemical:
-		$Sprite2D.scale = Vector2(0.2, 0.2)
+		$Sprite2D.scale = Vector2(1.0, 1.0)
 		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 		z_index = 1
 		global_position = get_global_mouse_position()
@@ -23,7 +23,7 @@ func _process(delta: float) -> void:
 		if Input.is_action_pressed("click") and not Global.hovering_shelf:
 			$Sprite2D.texture = spray_image
 	else:
-		$Sprite2D.scale = Vector2(0.125, 0.125)
+		$Sprite2D.scale = Vector2(0.65, 0.65)
 		z_index = 0
 		global_position = shelf_position
 		var dist_x = abs(global_position.x - get_global_mouse_position().x)
